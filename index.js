3750a5bff65c2a08c5b012c94653ca47223d972c
stanislavBenet <stanislav.benet@gmail.com>
2022-11-06 13:01:47 +0200

    Add new variable

---
+++ index.js
@@ -0,0 +1,8 @@
+let agePerson = 23;
+let isAdult = true;
+let languageCoding = 'C++'
+let isHasPersonPets = false;
+console.log (agePerson)
+console.log (isAdult)
+console.log (languageCoding)
+console.log (isHasPersonPets)
